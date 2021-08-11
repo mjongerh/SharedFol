6d55458e5a310e1d228ae5d6fd5b714574128957
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-08-11 17:31:16 +0200

    u

--- CERN/Lc_BDT_test/Lc_BDT_test.C
+++ CERN/Lc_BDT_test/Lc_BDT_test.C
@@ -165,7 +165,7 @@
     // Read training and test data
     // (it is also possible to use ASCII format as input -> see TMVA Users Guide)
     TFile *inputSignal(0);
-    TString fnamesig = "/home/mjongerh/alice/LcBDTtest/input/Lc_signal_nocuts_50files.root";
+    TString fnamesig = "/home/mjongerh/alice/LcBDTtest/input/Lc_signal_nocuts_200files.root";
     if (!gSystem->AccessPathName( fnamesig )) {
        inputSignal = TFile::Open( fnamesig ); // check if file in local directory exists
     }
@@ -176,7 +176,7 @@
     std::cout << "--- TMVAClassification       : Using input file: " << inputSignal->GetName() << std::endl;
 
     TFile *inputBackground(0);
-    TString fnamebkg = "/home/mjongerh/alice/LcBDTtest/input/Lc_background_nocuts_50files.root";
+    TString fnamebkg = "/home/mjongerh/alice/LcBDTtest/input/Lc_background_nocuts_200files.root";
     if (!gSystem->AccessPathName(fnamebkg)) {
       inputBackground = TFile::Open(fnamebkg); // check if file in local directory exists
     }
@@ -306,7 +306,7 @@
     //
     //    dataloader->PrepareTrainingAndTestTree( mycut,
     //         "NSigTrain=3000:NBkgTrain=3000:NSigTest=3000:NBkgTest=3000:SplitMode=Random:!V" );
-    dataloader->PrepareTrainingAndTestTree(mycuts, 905, 30000, 905, 30000, "SplitMode=Random:NormMode=NumEvents:!V" );
+    dataloader->PrepareTrainingAndTestTree(mycuts, 1500, 30000, 1500, 30000, "SplitMode=Random:NormMode=NumEvents:!V" );
 
     // ### Book MVA methods
     //
