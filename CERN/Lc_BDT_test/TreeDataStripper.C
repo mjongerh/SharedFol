413f9e65510812c41bea3e015193e0a993fe609e
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-08-23 13:28:09 +0200

    u

---
+++ CERN/Lc_BDT_test/TreeDataStripper.C
@@ -0,0 +1,28 @@
+ #include <cstdlib>
+ #include <iostream>
+ #include <map>
+ #include <string>
+
+ #include "TChain.h"
+ #include "TFile.h"
+ #include "TTree.h"
+ #include "TString.h"
+ #include "TObjString.h"
+ #include "TSystem.h"
+ #include "TROOT.h"
+
+void TreeDataStripper() {
+  TString oldfileName = "/home/mjongerh/alice/Run3Analysisvalidation/codeHF/AnalysisResults_trees_O2.root";
+  TString newfileName = "/home/mjongerh/alice/Run3Analysisvalidation/codeHF/AnalysisResults_trees_O2_stripped.root";
+
+  TFile oldFile(oldfilename);
+  oldFile->cd("DF_0");
+  TTree* oldtree;
+  oldFile.GetObject("T", oldtree);
+
+  TFile newFile(newfilename);
+  auto newtree = oldtree->CloneTree();
+
+  newtree->Print();
+  newFile.Write();
+}
