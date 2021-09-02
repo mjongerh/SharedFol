c82a34d5289488f10e270a7ecf294ea7988ec135
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-09-02 08:20:57 +0200

    u

---
+++ CERN/Lc_BDT_test/Pt_binner.C
@@ -0,0 +1,55 @@
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TClass.h"
+
+void Pt_binner()
+{
+  TString oldfileName = "/home/mjongerh/alice/Run3Analysisvalidation/codeHF/AnalysisResults_trees_O2_stripped.root";
+  TString newfileDir = "/home/mjongerh/alice/Run3Analysisvalidation/codeHF/PtBinTest/";
+
+  // PtBins - settings
+  const Int_t nPtBins = 6;
+  Float_t ptBins[nPtBins + 1] = {0., 1., 2., 4., 6., 8., 12.};
+
+  TFile oldFile(oldfileName);
+  TTree* oldtree;
+
+  TString objectstring = "O2hfcandp3full";
+  oldFile.GetObject(objectstring, oldtree);
+  if (oldtree == nullptr) {
+      printf("tree not found");
+  }
+  Long64_t nentries = oldtree->GetEntries();
+  float PtEntry;
+  oldtree->SetBranchAddress("fPt", &PtEntry);
+
+for (int i = 0; i < nPtBins; i++) {
+  Float_t PtLow = ptBins[i];
+  Float_t PtHigh = ptBins[i + 1];
+
+    TString newfileName = newfileDir + form("Lc_binTest_Pt%d", PtLow);
+    TFile newFile(newfileName, "RECREATE");
+    TTree* newtree = oldtree->CloneTree(0);
+    newtree->SetName("O2hfcandp3full");
+
+    for (Long64_t i = 0; i < nentries; i++) {
+      oldtree->GetEntry(i);
+      if (PtEntry >= PtLow && PtEntry < PtHigh)
+        newtree->Fill();
+    }
+    newFile.Write();
+}
+
+  
+
+}
