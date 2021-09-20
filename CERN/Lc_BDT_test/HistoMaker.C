c2d41e7562080846427207075c89ee91179a0c99
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-09-20 14:10:15 +0200

    u

--- CERN/Lc_BDT_test/HistoMaker.C
+++ CERN/Lc_BDT_test/HistoMaker.C
@@ -68,7 +68,7 @@ void HistoMaker() {
   }
   weight = Nbkg / Nsig;
 
-  TH2D* hDecayLengthS = new TH2D("hDecayLengthS", "Decay length vs BDT response", 20, 1.0, 0.0, 20, 1.0, 0.0); // 50, -0.8, 0.5, 50, 0, 30);
+  TH2D* hDecayLengthS = new TH2D("hDecayLengthS", "Decay length vs BDT response", 10, 1.0, 0.0, 10, 1.0, 0.0); // 50, -0.8, 0.5, 50, 0, 30);
   TH2D* hDecayLengthB = new TH2D("hDecayLengthB", "Decay length vs BDT response", 50, -0.8, 0.5, 50, 0, 30);
   TH2D* hImpactParameter0S = new TH2D("hImpactParameter0S", "Decay length vs BDT response", 50, -0.8, 0.5, 50, -1.0, 1.0);
   TH2D* hImpactParameter0B = new TH2D("hImpactParameter0B", "Decay length vs BDT response", 50, -0.8, 0.5, 50, -1.0, 1.0);
