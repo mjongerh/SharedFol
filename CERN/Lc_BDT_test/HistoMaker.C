eae376a67a77c7b1df3378ecbe43809da9afa6d7
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-09-20 14:14:46 +0200

    u

--- CERN/Lc_BDT_test/HistoMaker.C
+++ CERN/Lc_BDT_test/HistoMaker.C
@@ -114,7 +114,7 @@ void HistoMaker() {
     }
     c1->cd(2);
     //gStyle->SetPalette(30);
-    hDecayLengthS->Draw("colz");
+    hDecayLengthB->Draw("colz");
     //gStyle->SetPalette(20);
     //hDecayLengthB->Draw("colSAME");
 
