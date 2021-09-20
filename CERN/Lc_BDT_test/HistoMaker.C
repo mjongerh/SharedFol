79b36601b64fb4a1c1f75ae6edfbbfb6a9e2ca63
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2021-09-20 12:47:08 +0200

    u

--- CERN/Lc_BDT_test/HistoMaker.C
+++ CERN/Lc_BDT_test/HistoMaker.C
@@ -18,14 +18,16 @@ void HistoMaker() {
   TH1D* oldhist = (TH1D*)oldFile->Get("dataset/Method_BDT/BDT/MVA_BDT_effBvsS");
 
 
-  Double_t x[100], y[100]; //x: false positive (background eff), y: True positive (signal eff)
+  Double_t x[101], y[101]; //x: false positive (background eff), y: True positive (signal eff)
   Int_t n = 100;
   for (Int_t i = 0; i < n; i++) {
     x[i] = oldhist->GetBinContent(i+1);
     y[i] = oldhist->GetBinCenter(i+1);
   }
+  x[100] = 1.0;
+  y[100] = 1.0;
   TCanvas* c1 = new TCanvas("c1", "A Simple Graph Example", 200, 10, 500, 300);
-  TGraph* gr = new TGraph(n, x, y);
+  TGraph* gr = new TGraph(n+1, x, y);
   gr->GetXaxis()->SetTitle("False positive rate (bkg eff.)");
   gr->GetYaxis()->SetTitle("True positive rate (signal eff.)");
   gr->Draw("AC");
