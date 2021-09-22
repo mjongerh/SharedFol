997edf49e5e38e5753ea7c6a6518b495932b4399
mjongerh <mjongerh@live.nl>
2021-09-22 14:34:42 +0200

    u

--- GitRebuildAll.sh
+++ GitRebuildAll.sh
@@ -1,4 +1,6 @@
 cd /data/alice/mjongerh/alice
 aliBuild build AliPhysics --defaults o2
 aliBuild build O2 --defaults o2 --disable DebugGUI
+aliBuild build O2Physics --defaults o2
+aliBuild clean
 cd /data/alice/mjongerh/alice/SharedFol
