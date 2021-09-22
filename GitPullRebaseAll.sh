aa398b8473d25b894b037a9f9b9aa51f74377ef1
mjongerh <mjongerh@live.nl>
2021-09-22 14:32:32 +0200

    u

--- GitPullRebaseAll.sh
+++ GitPullRebaseAll.sh
@@ -1,11 +1,13 @@
 cd /data/alice/mjongerh/alice/SharedFol
 git pull --rebase
-cd /data/alice/mjongerh/alice/AliceO2
+cd /data/alice/mjongerh/alice/O2
 git pull --rebase
 cd /data/alice/mjongerh/alice/AliPhysics
 git pull --rebase
 cd /data/alice/mjongerh/alice/alidist
 git pull --rebase
 cd /data/alice/mjongerh/alice/Run3Analysisvalidation
+git pull --rebase
+cd /data/alice/mjongerh/alice/O2Physics
 git pull --rebase
 cd /data/alice/mjongerh/alice/SharedFol
