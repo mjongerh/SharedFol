a9a66ef0dfa695c6ee8489f3110bf8ed57065dcf
mjongerh <mjongerh@live.nl>
2021-11-24 11:01:06 +0100

    s

--- CERN/Lb/Test.C
+++ CERN/Lb/Test.C
@@ -12,11 +12,24 @@
 #include "TROOT.h"
 
 void Test() {
-    gROOT->SetBatch(kTRUE);
-    //TFile oldFile("./output/Pt5.0/TMVA.root");
-    TMVA::mvas("./output/Pt5.0/dataset/dataset", "./output/Pt5.0/TMVA.root", static_cast<TMVA::HistType>(3));
-    gROOT->SetBatch(kTRUE);
-    //void TMVA::mvaeffs(TString dataset, TString fin, Float_t nsignal, Float_t nbackground, Bool_t useTMVAStyle, TString formula)
-    TMVA::mvaeffs("./output/Pt5.0/dataset/dataset", "./output/Pt5.0/TMVA.root", 100.0, 1000.0, kTRUE, "S/sqrt(S+B)");
+  TFile finput("~/Desktop/ALICE3_HFperformance/analysis/foutputLambda_b.root"); 
+  TH1F* hbkg = (TH1F*)finput.Get("hBkgPerEvent");
+  TH1F* hsig = (TH1F*)finput.Get("histosignal");
+  Double_t PtBins[11] = {0., 1., 2., 3., 4., 5., 7., 10., 13., 16., 20., 24.};
+    Int_t Nevents = 210000000000000
+  for (Int_t i = 0; i < 11; i++) {
+    cout << "Pt " << PtBins[i] << endl
+         << " sig " << hsig->GetBinContent(i) << endl
+         << " bkg " << hbkg->GetBinContent(i)*Nevents << endl;
+  }
+    
+    
+    // test to use mvaeffs in a macro
+    //gROOT->SetBatch(kTRUE);
+    ////TFile oldFile("./output/Pt5.0/TMVA.root");
+    //TMVA::mvas("./output/Pt5.0/dataset/dataset", "./output/Pt5.0/TMVA.root", static_cast<TMVA::HistType>(3));
+    //gROOT->SetBatch(kTRUE);
+    ////void TMVA::mvaeffs(TString dataset, TString fin, Float_t nsignal, Float_t nbackground, Bool_t useTMVAStyle, TString formula)
+    //TMVA::mvaeffs("./output/Pt5.0/dataset/dataset", "./output/Pt5.0/TMVA.root", 100.0, 1000.0, kTRUE, "S/sqrt(S+B)");
   
 }
