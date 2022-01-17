aefaf1e29cde811dd1f6a76bffa163be5cbfa620
mjongerh <mjongerh@live.nl>
2022-01-17 14:46:02 +0100

    s

--- CERN/Lb/Lb_BDT.C
+++ CERN/Lb/Lb_BDT.C
@@ -57,7 +57,7 @@ int Lb_BDT(TString myMethodList = ""){
   Float_t ptBins[nPtBins + 1] = {0., 0.5, 1., 2., 3., 4., 5., 7., 10., 13., 16., 20., 24.};
   Float_t TrainFraction = 0.7; //fraction of training and testing. 0.7 is 70% training 30% testing
 
-  for (Int_t i = 5; i < nPtBins; i++) { //master loop over all ptbins
+  for (Int_t i = 5; i < 6; i++) { //nPtBins //master loop over all ptbins
     TFile* inputSignal(0);
     TString fnamesig = Form("/home/mjongerh/LbcutStrict_data/Trees/Lb_binned_signal_Pt%.1f.root", ptBins[i]); //Signal Input folder      /home/mjongerh/Lb_data/Trees/
     if (!gSystem->AccessPathName(fnamesig)) {
@@ -135,10 +135,11 @@ int Lb_BDT(TString myMethodList = ""){
     dataloader->AddVariable("fDecayLengthXY", "fDecayLengthXY", "units", 'F');
     dataloader->AddVariable("fImpactParameter0", "fImpactParameter0", "units", 'F');
     dataloader->AddVariable("fImpactParameter1", "fImpactParameter1", "units", 'F');
-    dataloader->AddVariable("fPtProng0", "fPtProng0", "units", 'F');
-    dataloader->AddVariable("fPtProng1", "fPtProng1", "units", 'F');
+
 
     //Spectator variables
+    dataloader->AddSpectator("fPtProng0", "fPtProng0", "units", 'F');
+    dataloader->AddSpectator("fPtProng1", "fPtProng1", "units", 'F');
     dataloader->AddSpectator("fM", "fM", "units", 'F');
     dataloader->AddSpectator("fPt", "fPt", "Gev", 'F');
 
