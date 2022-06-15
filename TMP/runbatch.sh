6b6356e06a0f699bf5a3d7c3e7b6d65fd4712fdc
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2022-06-15 14:57:06 +0200

    s

--- TMP/runbatch.sh
+++ TMP/runbatch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #export ALIBUILD_WORK_DIR="/data/alice/mjongerh/alice/sw"
 source /data/alice/mjongerh/alice/sw
-eval $(DelphesO2/latest-master-o2)
 cd /data/alice/mjongerh/alice/DelphesO2/examples/scripts
+
+eval $(alienv printenv DelphesO2/latest-master-o2)
+
 echo ${TMPDIR}
 echo ${PBS_O_WORKDIR}
 
@@ -10,7 +12,7 @@ outdir=${PBS_O_WORKDIR}/job_${PBS_ARRAYID}
 
 mkdir -p ${outdir}
 
-./createO2tables.py default_configfile.ini --entry BBBAR_BDforced -l -c --output-path /data/alice/mjongerh/alice/data/BBbar_test//job_${PBS_ARRAYID} --nruns 1 --njobs 1 --nevents 500
+./createO2tables.py default_configfile.ini --entry BBBAR_BDforced -l -c --output-path ${outdir} --nruns 1 --njobs 1 --nevents 500
 echo "Doing master cleanup"
 
 echo "Anything left?" 
