5d6c693d5ce37727b5fe7e50b49b0ea7285f848e
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2022-06-15 17:42:40 +0200

    s

--- TMP/submit.sh
+++ TMP/submit.sh
@@ -9,7 +9,7 @@ fi
 scriptdir=/data/alice/mjongerh/alice/DelphesO2/examples/scripts/
 for i in {1..2}
 do
-	runbatch.sh ${outdir}runbatch${i}.sh
+	cp runbatch.sh ${outdir}runbatch${i}.sh
 	cd ${outdir}
 	echo ${i}
 	qsub -o ${outdir}/logs/logOut -e ${outdir}/logs/logErr -q short runbatch${i}.sh
