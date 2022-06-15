7f7e6909a8001bfec60e992dc963786d73d73daa
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2022-06-15 14:38:05 +0200

    s

--- TMP/runbatch.sh
+++ TMP/runbatch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-eval $(alienv printenv VO_ALICE@O2::nightly-20210710-1)
-cd ${TMPDIR}
+#export ALIBUILD_WORK_DIR="/data/alice/mjongerh/alice/sw"
+source /data/alice/mjongerh/alice/sw
+eval $(DelphesO2/latest-master-o2)
+cd /data/alice/mjongerh/alice/DelphesO2/examples/scripts
 echo ${TMPDIR}
 echo ${PBS_O_WORKDIR}
 
@@ -8,60 +10,9 @@ outdir=${PBS_O_WORKDIR}/job_${PBS_ARRAYID}
 
 mkdir -p ${outdir}
 
-cp ${PBS_O_WORKDIR}/run_stratrack.sh ${TMPDIR}
-cp ${PBS_O_WORKDIR}/config_custom_Pythia.cfg ${TMPDIR}
-cp ${PBS_O_WORKDIR}/configPythia.ini ${TMPDIR}
-cp ${PBS_O_WORKDIR}/generator_pythia8_gun.C ${TMPDIR}
-cp ${PBS_O_WORKDIR}/RecoDecay.h ${TMPDIR}
-cp ${PBS_O_WORKDIR}/pp13.cmnd ${TMPDIR}
-cp ${PBS_O_WORKDIR}/run_strangenesstracking_01.C ${TMPDIR}
-cp ${PBS_O_WORKDIR}/trigger_multiplicity.C ${TMPDIR}
-
-source run_stratrack.sh
-root.exe -q -b run_strangenesstracking_01.C+
-if [ ! -f histooutput.root ]; then
-        echo "Failed, trying again"
-        root.exe -q -b run_strangenesstracking_01.C+
-fi
-if [ ! -f histooutput.root ]; then
-        echo "Failed yet again. Third time's the charm"
-        root.exe -q -b run_strangenesstracking_01.C+
-fi
-if [ ! -f histooutput.root ]; then
-        echo "Failed yet again. Third time's the charm"
-        root.exe -q -b run_strangenesstracking_01.C+
-fi
-if [ ! -f histooutput.root ]; then
-        echo "Forget it. Clean tree output up, this is bogus, sorry"
-        rm treeoutput.root
-fi
+./createO2tables.py default_configfile.ini --entry BBBAR_BDforced -l -c --output-path /data/alice/mjongerh/alice/data/BBbar_test//job_${PBS_ARRAYID} --nruns 1 --njobs 1 --nevents 500
 echo "Doing master cleanup"
-rm o2sim_Kine.root
-rm o2sim_HitsTRK.root
-rm o2sim.root
-rm run_strangenesstracking_01_C.d
-rm run_strangenesstracking_01_C.so
-rm o2sim_par.root
-rm o2sim_geometry.root
-rm o2sim_MCHeader.root
-rm o2sim_configuration.ini
-rm run_strangenesstracking_01_C_ACLiC_dict_rdict.pcm
-rm o2sim_grp.root
-rm MCStepLoggerVolMap.dat
-rm o2sim_proc-cut.dat
-rm MCStepLoggerSenVol.dat
-rm gphysi.dat
-rm run_stratrack.sh
-rm config_custom_Pythia.cfg
-rm configPythia.ini
-rm generator_pythia8_gun.C
-rm RecoDecay.h
-rm pp13.cmnd
-rm run_strangenesstracking_01.C
-rm pp13.cmnd
-rm trigger_multiplicity.C
-rm histooutput.root
-mv treeoutput.root ${outdir}
+
 echo "Anything left?" 
 ls
 echo "Done! Enjoy!"
--- TMP/submit.sh
+++ TMP/submit.sh
@@ -4,20 +4,13 @@ then
   exit 
 fi
 
-outdir=/dcache/alice/hohbern/${1}/
+outdir=/dcache/alice/mjongerh/${1}/
 if [ ! -d ${outdir} ]
 then
   mkdir -p ${outdir}
   mkdir -p ${outdir}/logs/
 fi
-cp run_stratrack.sh ${outdir}
-cp config_custom_Pythia.cfg ${outdir}
-cp configPythia.ini ${outdir}
-cp generator_pythia8_gun.C ${outdir}
-cp RecoDecay.h ${outdir}
-cp pp13.cmnd ${outdir}
-cp run_strangenesstracking_01.C ${outdir}
-cp trigger_multiplicity.C ${outdir}
+
 cp runbatch.sh ${outdir}
 cd ${outdir}
 qsub -o ${outdir}/logs/logOut -e ${outdir}/logs/logErr -q short -t ${2}-${3} runbatch.sh
