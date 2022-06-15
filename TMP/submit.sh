518d6b76b6308c1617036d41f8c4531eed67ae4c
Maurice Jongerhuis <m.v.jongerhuis@students.uu.nl>
2022-06-15 17:34:56 +0200

    s

--- TMP/submit.sh
+++ TMP/submit.sh
@@ -6,12 +6,13 @@ then
   mkdir -p ${outdir}/logs/
 fi
 
-cp runbatch.sh ${outdir}
-cd ${outdir}
+
 for i in {1..2}
 do
+	cp runbatch.sh ${outdir}runbatch${i}.sh
+	cd ${outdir}
 	echo ${i}
-	qsub -o ${outdir}/logs/logOut -e ${outdir}/logs/logErr -q short runbatch.sh
+	qsub -o ${outdir}/logs/logOut -e ${outdir}/logs/logErr -q short runbatch${i}.sh
 done
 
 cd -
