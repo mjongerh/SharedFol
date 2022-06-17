16ee307da2ec50caa354d21697a02c86e972bdc3
mjongerh <mjongerh@live.nl>
2022-06-17 10:29:38 +0200

    s

--- TMP/MoveOutput.sh
+++ TMP/MoveOutput.sh
@@ -1,17 +1,18 @@
 outdir=/data/alice/mjongerh/alice/data/pp14TeV_bbbar_50M_2T/
 j=0
-for i in {10460010..10460011} #83
+for i in {10460010..10460083} #83
 do
 	#statements
-	for f in {0..2} #199
+	for f in {0..199} #199
 	do
 	#statements
 	echo ${outdir}Job_${i}/AODRun5.${f}.root
 	echo ${outdir}AODRun5.${j}.root
 	if [ -f ${outdir}Job_${i}/AODRun5.${f}.root ]; then 
-        	cp ${outdir}Job_${i}/AODRun5.${f}.root ${outdir}AODRun5.${j}.root
+        	mv ${outdir}Job_${i}/AODRun5.${f}.root ${outdir}AODRun5.${j}.root
 			let j+=1
 	fi
     done
-
+	rm ${outdir}Job_${i}/summary.txt
+	rm ${outdir}Job_${i}/
 done
