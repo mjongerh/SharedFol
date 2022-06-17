b607a4122218d280afa84c9db8ea7ba50b6a4ae2
mjongerh <mjongerh@live.nl>
2022-06-17 10:23:31 +0200

    s

--- TMP/MoveOutput.sh
+++ TMP/MoveOutput.sh
@@ -6,6 +6,8 @@ do
 	for f in {0..2} #199
 	do
 	#statements
+	echo ${outdir}/Job_${i}/AODRun5.${f}.root
+	echo ${outdir}AODRun5.${j}.root
 	if [ ! -f ${outdir}/Job_${i}/AODRun5.${f}.root ]; then 
         	cp ${outdir}/Job_${i}/AODRun5.${f}.root ${outdir}AODRun5.${j}.root
 			let j+=1
